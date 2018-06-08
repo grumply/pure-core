bc887ebdebb60cf02ec7444700e7ff55c969e718
grumply <sean@grump.ly>
2018-06-08 10:44:23 -0500

    Reverse the roles of Add/Set feature combinators; default to add.

--- src/Pure/Data/View/Patterns.hs
+++ src/Pure/Data/View/Patterns.hs
@@ -8,19 +8,18 @@ module Pure.Data.View.Patterns
   , pattern Null
   , pattern Raw
   , pattern Keyed
-  , HasFeatures(..), pattern Features, pattern AddFeatures
-  , pattern Class, pattern Classes, pattern AddClasses
-  , pattern Style, pattern Styles, pattern AddStyles
-  , pattern Property, pattern Properties, pattern AddProperties
-  , pattern Attribute, pattern Attributes, pattern AddAttributes
-  , pattern Lifecycle, pattern Lifecycles, pattern AddLifecycles
-  , pattern Listener, pattern Listeners, pattern AddListeners
-  , HasXLinks(..), pattern XLink, pattern XLinks, pattern AddXLinks
-  , HasChildren(..), pattern Children, pattern AddChildren
-  , HasKeyedChildren(..), pattern KeyedChildren, pattern AddKeyedChildren
+  , HasFeatures(..), pattern Features, pattern SetFeatures
+  , pattern Class, pattern Classes, pattern SetClasses
+  , pattern Style, pattern Styles, pattern SetStyles
+  , pattern Property, pattern Properties, pattern SetProperties
+  , pattern Attribute, pattern Attributes, pattern SetAttributes
+  , pattern Lifecycle, pattern Lifecycles, pattern SetLifecycles
+  , pattern Listener, pattern Listeners, pattern SetListeners
+  , HasXLinks(..), pattern XLink, pattern XLinks, pattern SetXLinks
+  , HasChildren(..), pattern Children, pattern SetChildren
+  , HasKeyedChildren(..), pattern KeyedChildren, pattern SetKeyedChildren
   , (<|), (<||>), (|>)
   , (<||#>), (|#>)
-  , addProperty
   ) where
 
 -- This module exposes some hacky patterns due to GHCs lack of unidirectional expression patterns.
@@ -164,13 +163,13 @@ instance HasFeatures Features where
   setFeatures = const
   addFeatures = (<>)
 
+pattern SetFeatures :: HasFeatures a => Features -> a -> a
+pattern SetFeatures fs a <- ((getFeatures &&& id) -> (fs,a)) where
+  SetFeatures fs a = setFeatures fs a
+
 pattern Features :: HasFeatures a => Features -> a -> a
 pattern Features fs a <- ((getFeatures &&& id) -> (fs,a)) where
-  Features fs a = setFeatures fs a
-
-pattern AddFeatures :: HasFeatures a => Features -> a -> a
-pattern AddFeatures fs a <- ((getFeatures &&& id) -> (fs,a)) where
-  AddFeatures fs a = addFeatures fs a
+  Features fs a = addFeatures fs a
 
 -- Classes
 
@@ -181,16 +180,16 @@ pattern Class c a <- ((const "" &&& id) -> (c,a)) where
         cs = Set.insert c (classes fs)
     in setFeatures fs { classes = cs } a
 
-pattern Classes :: HasFeatures a => [Txt] -> a -> a
-pattern Classes cs a <- (((Set.toList . classes . getFeatures) &&& id) -> (cs,a)) where
-  Classes cs a =
+pattern SetClasses :: HasFeatures a => [Txt] -> a -> a
+pattern SetClasses cs a <- (((Set.toList . classes . getFeatures) &&& id) -> (cs,a)) where
+  SetClasses cs a =
     let cs' = foldr Set.insert Set.empty cs
         fs  = getFeatures a
     in setFeatures fs { classes = cs' } a
 
-pattern AddClasses :: HasFeatures a => [Txt] -> a -> a
-pattern AddClasses cs a <- Classes cs a where
-  AddClasses cs a =
+pattern Classes :: HasFeatures a => [Txt] -> a -> a
+pattern Classes cs a <- SetClasses cs a where
+  Classes cs a =
     let fs = getFeatures a
     in setFeatures (fs { classes = Set.union (Set.fromList cs) (classes fs) }) a
 
@@ -202,13 +201,13 @@ pattern Style k v a <- ((const ("","") &&& id) -> ((k,v),a)) where
     let fs = getFeatures a
     in setFeatures (fs { styles = Map.insert k v (styles fs) }) a
 
-pattern Styles :: HasFeatures a => [(Txt,Txt)] -> a -> a
-pattern Styles ss v <- (((Map.toList . styles . getFeatures) &&& id) -> (ss,v)) where
-  Styles ss v = setFeatures ((getFeatures v) { styles = Map.fromList ss }) v
+pattern SetStyles :: HasFeatures a => [(Txt,Txt)] -> a -> a
+pattern SetStyles ss v <- (((Map.toList . styles . getFeatures) &&& id) -> (ss,v)) where
+  SetStyles ss v = setFeatures ((getFeatures v) { styles = Map.fromList ss }) v
 
-pattern AddStyles :: HasFeatures a => [(Txt,Txt)] -> a -> a
-pattern AddStyles ss v <- Styles ss v where
-  AddStyles ss v =
+pattern Styles :: HasFeatures a => [(Txt,Txt)] -> a -> a
+pattern Styles ss v <- SetStyles ss v where
+  Styles ss v =
     let fs = getFeatures v
     in setFeatures (fs { styles = Map.union (Map.fromList ss) (styles fs) }) v
 
@@ -220,13 +219,13 @@ pattern Listener l a <- ((const (On "" ElementTarget def (\_ -> return ()) (retu
     let fs = getFeatures a
     in setFeatures (fs { listeners = l : listeners fs }) a
 
-pattern Listeners :: HasFeatures a => [Listener] -> a -> a
-pattern Listeners ls v <- (((listeners . getFeatures) &&& id) -> (ls,v)) where
-  Listeners ls v = setFeatures ((getFeatures v) { listeners = ls }) v
+pattern SetListeners :: HasFeatures a => [Listener] -> a -> a
+pattern SetListeners ls v <- (((listeners . getFeatures) &&& id) -> (ls,v)) where
+  SetListeners ls v = setFeatures ((getFeatures v) { listeners = ls }) v
 
-pattern AddListeners :: HasFeatures a => [Listener] -> a -> a
-pattern AddListeners ls v <- Listeners ls v where
-  AddListeners ls v =
+pattern Listeners :: HasFeatures a => [Listener] -> a -> a
+pattern Listeners ls v <- SetListeners ls v where
+  Listeners ls v =
     let fs = getFeatures v
     in setFeatures (fs { listeners = ls ++ listeners fs }) v
 
@@ -238,13 +237,13 @@ pattern Attribute k v a <- ((const ("","") &&& id) -> ((k,v),a)) where
     let fs = getFeatures a
     in setFeatures (fs { attributes = Map.insert k v (attributes fs) }) a
 
-pattern Attributes :: HasFeatures a => [(Txt,Txt)] -> a -> a
-pattern Attributes as v <- (((Map.toList . attributes . getFeatures) &&& id) -> (as,v)) where
-  Attributes as v = setFeatures ((getFeatures v) { attributes = Map.fromList as }) v
+pattern SetAttributes :: HasFeatures a => [(Txt,Txt)] -> a -> a
+pattern SetAttributes as v <- (((Map.toList . attributes . getFeatures) &&& id) -> (as,v)) where
+  SetAttributes as v = setFeatures ((getFeatures v) { attributes = Map.fromList as }) v
 
-pattern AddAttributes :: HasFeatures a => [(Txt,Txt)] -> a -> a
-pattern AddAttributes as v <- Attributes as v where
-  AddAttributes as v =
+pattern Attributes :: HasFeatures a => [(Txt,Txt)] -> a -> a
+pattern Attributes as v <- SetAttributes as v where
+  Attributes as v =
     let fs = getFeatures v
     in setFeatures (fs { attributes = Map.union (Map.fromList as) (attributes fs) }) v
 
@@ -258,22 +257,16 @@ pattern Property k v a <- ((const ("","") &&& id) -> ((k,v),a)) where
         ps' = Map.insert k v ps
     in setFeatures fs { properties = ps' } a
 
-addProperty :: (Txt,Txt) -> Features -> Features
-addProperty (k,v) fs =
-  let ps = properties fs
-      ps' = Map.insert k v ps
-  in fs { properties = ps' }
-
-pattern Properties :: HasFeatures a => [(Txt,Txt)] -> a -> a
-pattern Properties ps v <- (((Map.toList . properties . getFeatures) &&& id) -> (ps,v)) where
-  Properties ps v =
+pattern SetProperties :: HasFeatures a => [(Txt,Txt)] -> a -> a
+pattern SetProperties ps v <- (((Map.toList . properties . getFeatures) &&& id) -> (ps,v)) where
+  SetProperties ps v =
     let fs = getFeatures v
         ps' = foldr (\(k,v) -> Map.insert k v) Map.empty ps
     in setFeatures fs { properties = ps' } v
 
-pattern AddProperties :: HasFeatures a => [(Txt,Txt)] -> a -> a
-pattern AddProperties ps v <- Properties ps v where
-  AddProperties ps v =
+pattern Properties :: HasFeatures a => [(Txt,Txt)] -> a -> a
+pattern Properties ps v <- SetProperties ps v where
+  Properties ps v =
     let fs = getFeatures v
     in setFeatures (fs { properties = Map.union (Map.fromList ps) (properties fs) }) v
 
@@ -285,13 +278,13 @@ pattern Lifecycle l a <- ((const (HostRef (\_ -> return ())) &&& id) -> (l,a)) w
     let fs = getFeatures a
     in setFeatures (fs { lifecycles = l : lifecycles fs }) a
 
+pattern SetLifecycles :: HasFeatures a => [Lifecycle] -> a -> a
+pattern SetLifecycles lc v <- (((lifecycles . getFeatures) &&& id) -> (lc,v)) where
+  SetLifecycles lc v = setFeatures ((getFeatures v) { lifecycles = lc }) v
+
 pattern Lifecycles :: HasFeatures a => [Lifecycle] -> a -> a
 pattern Lifecycles lc v <- (((lifecycles . getFeatures) &&& id) -> (lc,v)) where
-  Lifecycles lc v = setFeatures ((getFeatures v) { lifecycles = lc }) v
-
-pattern AddLifecycles :: HasFeatures a => [Lifecycle] -> a -> a
-pattern AddLifecycles lc v <- (((lifecycles . getFeatures) &&& id) -> (lc,v)) where
-  AddLifecycles lc v =
+  Lifecycles lc v =
     let fs = getFeatures v
     in setFeatures (fs { lifecycles = lc ++ lifecycles fs }) v
 
@@ -323,13 +316,13 @@ pattern XLink k v a <- ((const ("","") &&& id) -> ((k,v),a)) where
     let xls = getXLinks a
     in setXLinks ((k,v):xls) a
 
+pattern SetXLinks :: HasXLinks a => [(Txt,Txt)] -> a -> a
+pattern SetXLinks xl v <- ((getXLinks &&& id) -> (xl,v)) where
+  SetXLinks xl v = setXLinks xl v
+
 pattern XLinks :: HasXLinks a => [(Txt,Txt)] -> a -> a
 pattern XLinks xl v <- ((getXLinks &&& id) -> (xl,v)) where
-  XLinks xl v = setXLinks xl v
-
-pattern AddXLinks :: HasXLinks a => [(Txt,Txt)] -> a -> a
-pattern AddXLinks xl v <- ((getXLinks &&& id) -> (xl,v)) where
-  AddXLinks xl v = addXLinks xl v
+  XLinks xl v = addXLinks xl v
 
 -- Combinators
 
@@ -353,13 +346,13 @@ instance HasChildren View where
   addChildren cs PortalView {..} = PortalView { portalView = setChildren cs portalView, .. }
   addChildren _ v = v
 
+pattern SetChildren :: HasChildren a => [View] -> a -> a
+pattern SetChildren cs v <- ((getChildren &&& id) -> (cs,v)) where
+  SetChildren cs v = setChildren cs v
+
 pattern Children :: HasChildren a => [View] -> a -> a
 pattern Children cs v <- ((getChildren &&& id) -> (cs,v)) where
-  Children cs v = setChildren cs v
-
-pattern AddChildren :: HasChildren a => [View] -> a -> a
-pattern AddChildren cs v <- ((getChildren &&& id) -> (cs,v)) where
-  AddChildren cs v = addChildren cs v
+  Children cs v = addChildren cs v
 
 -- Keyed Children
 
@@ -383,13 +376,13 @@ instance HasKeyedChildren View where
   addKeyedChildren cs PortalView {..} = PortalView { portalView = setKeyedChildren cs portalView, .. }
   addKeyedChildren _ v = v
 
+pattern SetKeyedChildren :: HasKeyedChildren a => [(Int,View)] -> a -> a
+pattern SetKeyedChildren ks v <- ((getKeyedChildren &&& id) -> (ks,v)) where
+  SetKeyedChildren ks v = setKeyedChildren ks v
+
 pattern KeyedChildren :: HasKeyedChildren a => [(Int,View)] -> a -> a
 pattern KeyedChildren ks v <- ((getKeyedChildren &&& id) -> (ks,v)) where
-  KeyedChildren ks v = setKeyedChildren ks v
-
-pattern AddKeyedChildren :: HasKeyedChildren a => [(Int,View)] -> a -> a
-pattern AddKeyedChildren ks v <- ((getKeyedChildren &&& id) -> (ks,v)) where
-  AddKeyedChildren ks v = addKeyedChildren ks v
+  KeyedChildren ks v = addKeyedChildren ks v
 
 infixl 1 <|
 {-# INLINE (<|) #-}
