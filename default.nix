e89023bd3fbe6456e399f490b37e2bb66c628673
Sean Hickman <sean@grumply.com>
2020-02-14 00:03:06 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "pure-core";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [
     base containers pure-default pure-json pure-lifted
